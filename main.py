599db932c5fc40a0312ab34d98241f1b40e5f026
Supergmam02 <griffith.a.disabato@gmail.com>
2025-09-30 16:39:09 -0400

    added a function to main.py that prints a report on a book

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ def get_book_text(filename):
         contents = f.read()
     return contents
 
+def print_report(num_words, char_list):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in char_list:
+        if i["char"].isalpha():
+            print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
+
 def main():
     book = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book)
     char_count = get_char_count(book)
-    print(f"Found {num_words} total words")
-    print(char_count)
-    print(sort_characters(char_count))
+    sorted_char_count = sort_characters(char_count)
+    print_report(num_words, sorted_char_count)
 
 main()
