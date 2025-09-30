ab2e3af5e506fcc3f08cf559017be038336393b3
Supergmam02 <griffith.a.disabato@gmail.com>
2025-09-30 16:47:07 -0400

    updated main.py so that code can have path to books passed into it

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from stats import get_num_words
 from stats import get_char_count
 from stats import sort_characters
+import sys
 
 def get_book_text(filename):
     with open(filename) as f:
         contents = f.read()
     return contents
 
-def print_report(num_words, char_list):
+def print_report(num_words, char_list, book_path):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
@@ -19,10 +20,13 @@ def print_report(num_words, char_list):
     print("============= END ===============")
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = get_book_text(f"{sys.argv[1]}")
     num_words = get_num_words(book)
     char_count = get_char_count(book)
     sorted_char_count = sort_characters(char_count)
-    print_report(num_words, sorted_char_count)
+    print_report(num_words, sorted_char_count, sys.argv[1])
 
 main()
