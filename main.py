843d1030832d01b359fc7dd33adc87cc63e674b2
Supergmam02 <griffith.a.disabato@gmail.com>
2025-09-30 15:46:55 -0400

    updated main.py to read txt files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filename):
+    with open(filename) as f:
+        contents = f.read()
+    return contents
+
+def main():
+    st = get_book_text("books/frankenstein.txt")
+    print(st)
+
+main()
