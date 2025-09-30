bb2e031af51a65051aafe6f62a67b316836dcf47
Supergmam02 <griffith.a.disabato@gmail.com>
2025-09-30 15:59:09 -0400

    created stats.py and refactored word counting function into stats.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
+from stats import get_num_words
 def get_book_text(filename):
     with open(filename) as f:
         contents = f.read()
     return contents
 
-def count_words(text):
-    word_list = text.split()
-    count = len(word_list)
-    return count
-
 def main():
     book = get_book_text("books/frankenstein.txt")
-    num_words = count_words(book)
+    num_words = get_num_words(book)
     print(f"Found {num_words} total words")
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+    word_list = text.split()
+    count = len(word_list)
+    return count
